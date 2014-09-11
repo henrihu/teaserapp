81710cb409e69bb1372fbcfd8958106696e8211d
Javed43 <javed.hussain@mobiloitte.com>
2014-09-11 10:21:40 +0530

    Fix

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,7 +3,7 @@ class Video < ActiveRecord::Base
   has_many :favorites, :dependent => :destroy
   validates :name, presence: true, length: {  in: 3..20 }
   validates :genre_id, presence: :true
-  validates :director_name, presence: :true, length: {  in: 3..20 }
+  #validates :director_name, presence: :true, length: {  in: 3..20 }
   validates :link, :presence => true, format: { with: /(?:.be\/|\s+|\/watch\?v=|\/(?=p\/))([\w\/\-]+)/}#, url: { message: "Please follow this format (http://www.youtube.com/)."}				
   validates :year, presence: true
 end
