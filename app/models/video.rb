07bbaaa776786bd8b5f3923e8d9088524f89621d
Javed43 <javed.hussain@mobiloitte.com>
2014-09-25 17:30:51 +0530

    Message updated

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,8 +5,16 @@ class Video < ActiveRecord::Base
   validates :director_name, presence: :true, length: {  in: 3..100 }
   validates :link, :presence => true, format: { with: /(?:.be\/|\s+|\/watch\?v=|\/(?=p\/))([\w\/\-]+)/}#, url: { message: "Please follow this format (http://www.youtube.com/)."}				
   validates :year, presence: true
+  validate :require_at_least_one_genre
   has_many :users_videos
   has_many :users, through: :users_videos
   has_many :histories, :dependent => :destroy
   has_and_belongs_to_many :genres,  :join_table => :genres_videos
+
+  private
+
+	def require_at_least_one_genre
+	  errors.add(:genre_ids, "Please select atleast one genre.") if (genre_ids.length < 1)
+	end
+
 end
