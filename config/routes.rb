513be55fb50bb57ca4d13d9abef5358b294200df
Javed43 <javed.hussain@mobiloitte.com>
2015-01-03 16:13:47 +0530

    Information Page Integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  root :to => 'admin/users#index'
+  
+  root to: 'homes#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
