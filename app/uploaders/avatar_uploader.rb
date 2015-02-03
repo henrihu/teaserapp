21af924fdf7662f7b18bf3c47eb8f007f5aac9d8
Javed43 <javed.hussain@mobiloitte.com>
2015-02-03 19:26:30 +0530

    Cloudinay Added

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,7 +4,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
    include CarrierWave::RMagick
-  # include Cloudinary::CarrierWave
+   include Cloudinary::CarrierWave
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
